ae827a9626b9bf51a0b2cb925f5c8aec732b5c04
Javid Hajili <145369360+HajiliJavid@users.noreply.github.com>
2024-10-16 19:35:06 +0400

    console.js

--- Bonus Projects (15th October)/Note App/console.js
+++ Bonus Projects (15th October)/Note App/console.js
@@ -8,15 +8,10 @@ addButton.addEventListener("click", function(){
     cardsDiv.insertBefore(newCard, addButton);
 });
 
-cardsDiv.addEventListener("dblclick", function(){
-
-})
-
-
 cardsDiv.addEventListener("dblclick", (e) => {
     if (e.target.classList == 'note') {
-        confirm("Do you want to delete this note?");
-        if(confirm){
+        const message=confirm("Do you want to delete this note?");
+        if(message){
             e.target.remove();
         }
         else{
